543cf62788e106e750a07ceb3f2175bc7cd57bc1
GrougalQatal <71042478+GrougalQatal@users.noreply.github.com>
2020-09-17 18:51:03 -0500

    .....

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -12,41 +12,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _lista(),
-      // CustomScrollView(
-      //     slivers: [
-      //       SliverAppBar(
-      //         brightness: Brightness.light,
-      //         backgroundColor: Colors.white,
-      //         title: Text(
-      //           'DentCloud',
-      //           style: const TextStyle(
-      //             color: Palette.textColor,
-      //             fontSize: 28.0,
-      //             fontWeight: FontWeight.bold,
-      //             letterSpacing: -1.2,
-      //           ),
-      //         ),
-      //         centerTitle: false,
-      //         floating: true,
-      //         actions: [
-      //           CircleButton(
-      //             icon: Icons.search,
-      //             iconsize: 30.0,
-      //             onPressed: () => print('Search'),
-      //           ),
-      //           CircleButton(
-      //             icon: MdiIcons.facebookMessenger,
-      //             iconsize: 30.0,
-      //             onPressed: () => print('Messenger'),
-      //           )
-      //         ],
-      //       ),
-      //       SliverToBoxAdapter(
-      //         child: CreatePostContainer(currentUser: "Freddy"),
-      //       )
-      //     ],
-      //   ),
+      body: 
+      CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              brightness: Brightness.light,
+              backgroundColor: Colors.white,
+              title: Text(
+                'DentCloud',
+                style: const TextStyle(
+                  color: Palette.textColor,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2,
+                ),
+              ),
+              centerTitle: false,
+              floating: true,
+              actions: [
+                CircleButton(
+                  icon: Icons.search,
+                  iconsize: 30.0,
+                  onPressed: () => print('Search'),
+                ),
+                CircleButton(
+                  icon: MdiIcons.facebookMessenger,
+                  iconsize: 30.0,
+                  onPressed: () => print('Messenger'),
+                )
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: CreatePostContainer(currentUser: "Freddy"),
+            )
+          ],
+        ),
     );
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,14 +4,15 @@ import 'package:muro_dentcloud/src/pages/home_page.dart';
 import 'package:muro_dentcloud/src/pages/listview_page.dart';
 import 'package:muro_dentcloud/src/pages/signin.dart';
 import 'package:muro_dentcloud/src/pages/signup.dart';
+import 'package:muro_dentcloud/src/pages/startup_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => SignIn(),
-    'home': (BuildContext context) => HomePage(),
-    'gps': (BuildContext context) => ListaPage(),
-    'agenda': (BuildContext context) => CardPage(),
-    'perfil': (BuildContext context) => HomePage(),
-    'registrarse': (BuildContext context) => Signup(),
+    '/'            : (BuildContext context) => SignIn(),
+    'gps'          : (BuildContext context) => ListaPage(),
+    'home'         : (BuildContext context) => StartUpPage(),
+    'agenda'       : (BuildContext context) => CardPage(),
+    'perfil'       : (BuildContext context) => HomePage(),
+    'registrarse'  : (BuildContext context) => Signup(),
   };
 }
