0f87e0801256fddbf4cb1a80f8e43fc4e8f252b3
Seerendo <70487872+Seerendo@users.noreply.github.com>
2020-09-21 18:39:47 -0500

    Agenda - Parte 3
    Nuevo Modelo de Screens para la Agenda

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,15 +6,17 @@ import 'package:muro_dentcloud/src/pages/listview_page.dart';
 import 'package:muro_dentcloud/src/pages/signin.dart';
 import 'package:muro_dentcloud/src/pages/signup.dart';
 import 'package:muro_dentcloud/src/pages/startup_page.dart';
+import 'package:muro_dentcloud/src/widgets/add_event.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/'            : (BuildContext context) => CardPage(),
+    '/'            : (BuildContext context) => HomePage(),
     'gps'          : (BuildContext context) => ListaPage(),
     'home'         : (BuildContext context) => StartUpPage(),
     'agenda'       : (BuildContext context) => CardPage(),
     'perfil'       : (BuildContext context) => HomePage(),
     'registrarse'  : (BuildContext context) => Signup(),
     'agenda2'      : (BuildContext context) => Agenda(),
+    /*'addagenda'      : (BuildContext context) => AddEventPage(),*/
   };
 }
