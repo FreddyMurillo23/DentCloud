86473da079b46d96149e6c82db2dda967180ff6d
GrougalQatal <71042478+GrougalQatal@users.noreply.github.com>
2020-09-20 19:14:56 -0500

    .....

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:muro_dentcloud/src/providers/publicaciones_provider.dart';
+import 'package:muro_dentcloud/src/widgets/cards.dart';
 import 'package:provider/provider.dart';
 
 class CardPage extends StatefulWidget {
@@ -80,128 +81,9 @@ class _CardPageState extends State<CardPage> {
     return Container(
       child: Column(
         children: <Widget>[
-          _cardTipo2(imagen),
+          CardWidgetPublicaciones(imagen: imagen,),
           SizedBox(height: 30.0),
         ],
-      ),
-    );
-  }
-
-  Widget _cardTipo2(int imagen) {
-    final card = Container(
-      // clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: <Widget>[
-          Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white,
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                        color: Colors.grey[400],
-                        blurRadius: 10.0,
-                        spreadRadius: 2.0,
-                        offset: Offset(2.0, 10.0))
-                  ]),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Stack(
-                  alignment: Alignment(-0.95, 0.99),
-                  children: <Widget>[
-                    FadeInImage(
-                      image: NetworkImage(
-                          'https://picsum.photos/450/450/?image=$imagen'),
-                      placeholder: AssetImage('assets/jar-loading.gif'),
-                      fadeInDuration: Duration(milliseconds: 200),
-                      height: 450.0,
-                      fit: BoxFit.cover,
-                    ),
-                    InputChip(
-                        avatar: CircleAvatar(
-                            backgroundColor: Colors.black, child: Text('AB')),
-                        label: Text('Aaron Burr'),
-                        backgroundColor: Colors.grey[400],
-                        onPressed: () {
-                          print('I am the one thing in life.');
-                        }),
-                  ],
-                ),
-              )),
-          Divider(
-            height: 20,
-            thickness: 0,
-            color: Colors.transparent,
-          ),
-          ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blue),
-            title: Text('Soy el titulo de esta tarjeta'),
-            subtitle: Text(
-                'Aquí estamos con la descripción de la tajera que quiero que ustedes vean para tener una idea de lo que quiero mostrarles'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Row(
-                  children: [
-                    Icon(
-                      MdiIcons.thumbUpOutline,
-                      color: Colors.blue[400],
-                      size: 20.00,
-                    ),
-                    Text(' Me Gusta'),
-                  ],
-                ),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Row(
-                  children: [
-                    Icon(
-                      MdiIcons.commentOutline,
-                      color: Colors.blue[500],
-                      size: 20.00,
-                    ),
-                    Text(' Comentar'),
-                  ],
-                ),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Row(
-                  children: [
-                    Icon(
-                      MdiIcons.shareOutline,
-                      color: Colors.blue[300],
-                      size: 30.00,
-                    ),
-                    Text('Compartir'),
-                  ],
-                ),
-                onPressed: () {},
-              )
-            ],
-          ),
-        ],
-      ),
-    );
-
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30.0),
-          color: Colors.white,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.grey[500],
-                blurRadius: 10.0,
-                spreadRadius: 2.0,
-                offset: Offset(2.0, 10.0))
-          ]),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30.0),
-        child: Column(children: <Widget>[
-          card,
-        ]),
       ),
     );
   }
@@ -210,6 +92,7 @@ class _CardPageState extends State<CardPage> {
     for (var i = 1; i < 10; i++) {
       _ultimoItem++;
       if(_ultimoItem==97) _ultimoItem++;
+      if(_ultimoItem==207) _ultimoItem++;
       _listaNumeros.add(_ultimoItem);
     }
     setState(() {});
---
+++ lib/src/widgets/cards.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class CardWidgetPublicaciones extends StatelessWidget {
+  final int imagen;
+
+  const CardWidgetPublicaciones(
+    {Key key,
+    @required this.imagen,
+    
+    }) 
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.grey[500],
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white,
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                                color: Colors.grey[400],
+                                blurRadius: 10.0,
+                                spreadRadius: 2.0,
+                                offset: Offset(2.0, 10.0))
+                          ]),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Stack(
+                          alignment: Alignment(-0.95, 0.99),
+                          children: <Widget>[
+                            FadeInImage(
+                              image: NetworkImage(
+                                  'https://picsum.photos/450/450/?image=$imagen'),
+                              placeholder: AssetImage('assets/jar-loading.gif'),
+                              fadeInDuration: Duration(milliseconds: 200),
+                              height: 450.0,
+                              fit: BoxFit.cover,
+                            ),
+                            InputChip(
+                                avatar: CircleAvatar(
+                                    backgroundColor: Colors.black, child: Text('AB')),
+                                label: Text('Aaron Burr'),
+                                backgroundColor: Colors.grey[400],
+                                onPressed: () {
+                                  print('I am the one thing in life.');
+                                }),
+                          ],
+                        ),
+                      )),
+                  Divider(
+                    height: 20,
+                    thickness: 0,
+                    color: Colors.transparent,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.photo_album, color: Colors.blue),
+                    title: Text('Soy el titulo de esta tarjeta'),
+                    subtitle: Text(
+                        'Aquí estamos con la descripción de la tajera que quiero que ustedes vean para tener una idea de lo que quiero mostrarles'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlatButton(
+                        child: Row(
+                          children: [
+                            Icon(
+                              MdiIcons.thumbUpOutline,
+                              color: Colors.blue[400],
+                              size: 20.00,
+                            ),
+                            Text(' Me Gusta'),
+                          ],
+                        ),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: Row(
+                          children: [
+                            Icon(
+                              MdiIcons.commentOutline,
+                              color: Colors.blue[500],
+                              size: 20.00,
+                            ),
+                            Text(' Comentar'),
+                          ],
+                        ),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: Row(
+                          children: [
+                            Icon(
+                              MdiIcons.shareOutline,
+                              color: Colors.blue[300],
+                              size: 30.00,
+                            ),
+                            Text('Compartir'),
+                          ],
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
