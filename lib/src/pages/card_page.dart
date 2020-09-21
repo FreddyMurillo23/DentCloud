9a916d928ebecb1911ca11c67f13251b68ac8481
GrougalQatal <71042478+GrougalQatal@users.noreply.github.com>
2020-09-20 20:27:22 -0500

    Update card_page.dart

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -127,8 +127,11 @@ class _CardPageState extends State<CardPage> {
 // ? Condiciona la respuesta http para que los datos que se traigan tengan animacion y luego agrega 10 mas
   void respuestaHTTP() {
     _isLoading = false;
-    _scrollController.animateTo(_scrollController.position.pixels + 100,
-        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(milliseconds: 250)
+    );
     _agregar10();
   }
 
