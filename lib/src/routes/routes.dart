67d45944f42970a1933077fe03a8dface5ae1c52
Seerendo <70487872+Seerendo@users.noreply.github.com>
2020-10-24 18:39:00 -0500

    Commit 1

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,17 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:muro_dentcloud/src/pages/card_page.dart';
-import 'package:muro_dentcloud/src/pages/home_page.dart';
-import 'package:muro_dentcloud/src/pages/listview_page.dart';
-
-Map<String, WidgetBuilder> getApplicationRoutes() {
-  return <String, WidgetBuilder>{
-    '/': (BuildContext context) => HomePage(),
-    'home': (BuildContext context) => CardPage(),
-    'gps': (BuildContext context) => ListaPage(),
-    'agenda': (BuildContext context) => HomePage(),
-    'perfil': (BuildContext context) => HomePage(),
-=======
 import 'package:muro_dentcloud/src/pages/agenda/add_event.dart';
 import 'package:muro_dentcloud/src/pages/agenda/doctor_pendients.dart';
 import 'package:muro_dentcloud/src/pages/details_Patients.dart';
@@ -38,6 +25,5 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'patients': (BuildContext context) => ListPatientsBuild(),
     'pruebaUser': (BuildContext context) => DetailPage(),
     'eventosPendientes'    : (BuildContext context) => DoctorEventsPendients()
->>>>>>> Pruebas
   };
 }
