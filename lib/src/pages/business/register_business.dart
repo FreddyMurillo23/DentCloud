8fd418206e83db450f13433bad09960eb475847e
deivitrix <67457502+deivitrix@users.noreply.github.com>
2020-11-04 22:41:34 -0500

    Revert "update"
    This reverts commit aa00a30c50df32fac3bde3a6224c9373188952f5.

--- lib/src/pages/business/register_business.dart
+++ lib/src/pages/business/register_business.dart
@@ -1,9 +1,4 @@
-import 'dart:html';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-//import 'package:image_picker/image_picker.dart';
-
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:muro_dentcloud/src/widgets/circle_button.dart';
 
@@ -15,7 +10,7 @@ class RegisterBusiness extends StatefulWidget {
 }
 
 class _RegisterBusinessState extends State<RegisterBusiness> {
-  File imageFile;
+ 
   @override
   Widget build(BuildContext context) {
      final sizeScreen = MediaQuery.of(context).size;
@@ -30,52 +25,26 @@ class _RegisterBusinessState extends State<RegisterBusiness> {
     );
   }
 
- 
- _seleccionarFoto() async {
-   // var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
- }
+
 
 Widget fotoperfil(Size sizescreen)
 {
-  return  Padding(
-      padding: EdgeInsets.all(20),
-      child: Center(
-      child: Stack(
-        //fit:  StackFit.expand,
-        children: [
-          Container(
-          width: sizescreen.width*0.3,
-            height: sizescreen.height*0.2,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(40.0),
-                child: FadeInImage(
-                placeholder: AssetImage('assets/placeholder.png'),
-                 image: new NetworkImage("http://54.197.83.249/Contenido_ftp/Imagenes%20por%20defecto/Placeholder_male.png"),
-              fit: BoxFit.cover
-              ),
-            ),
-            decoration:  BoxDecoration(
-              shape: BoxShape.circle, 
-            ),
-     //color:  Colors.blue,
-     
+  /*return  Container(
+    decoration: BoxDecoration(
+      gradient:  LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+           Color(0XFF2E5596),
+          Color(0XFF16304E),
+        ]
+      ),
     ),
-    Positioned(
-         bottom: -11,
-         right: -15,
-          child: Container(
-        child: IconButton(
-          icon: Icon(Icons.add_a_photo),
-          onPressed: (){
+  height: sizescreen.height*0.10,
+  //color:  Colors.blue,
+  
 
-          },
-        ),
-      ),
-    ),
-    ],
-      ),
-      ),
-  );
+  );*/
 }
 
 
