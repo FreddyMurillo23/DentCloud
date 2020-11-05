a02079aeacb05f27f84004983cd9272b9640a447
deivitrix <67457502+deivitrix@users.noreply.github.com>
2020-11-05 16:57:08 -0500

    update

--- lib/src/pages/business/register_business.dart
+++ lib/src/pages/business/register_business.dart
@@ -20,7 +20,7 @@ class _RegisterBusinessState extends State<RegisterBusiness> {
       appBar: appMenu(sizeScreen),
       body: Column(
         children: [
-          fotoperfil(sizeScreen),
+          _mostrarfotoperfil(sizeScreen),
         ],
       ),
 
@@ -56,7 +56,7 @@ Widget fotoperfil(Size sizescreen)
                  child: IconButton(
                  icon: Icon(Icons.add_a_photo),
                  onPressed: (){
-                   _selecionarfoto();
+                   //_selecionarfoto();
                  },
                  ),
             ),
@@ -86,7 +86,7 @@ Widget fotoperfil(Size sizescreen)
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(40.0),
                 child: FadeInImage(
-                 placeholder: AssetImage('assets/jar-loading.gif'),
+                 //placeholder: AssetImage('assets/placeholder.png'),
                  image: FileImage(foto),
               fit: BoxFit.cover
               ),
@@ -102,7 +102,7 @@ Widget fotoperfil(Size sizescreen)
                  child: IconButton(
                  icon: Icon(Icons.add_a_photo),
                  onPressed: (){
-                   _selecionarfoto();
+                   //_selecionarfoto();
                  },
                  ),
             ),
@@ -116,6 +116,43 @@ Widget fotoperfil(Size sizescreen)
   }
   else
   {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Stack(
+         children: [
+           Container(
+             width: sizescreen.width*0.3,
+            height: sizescreen.height*0.2,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/placeholder.png'),
+                 image: AssetImage('assets/placeholder.png'),
+              fit: BoxFit.cover
+              ),
+            ),
+             decoration:  BoxDecoration(
+              shape: BoxShape.circle, 
+            ),
+           ),
+           Positioned(
+              bottom: -11,
+              right: -15,
+               child: Container(
+                 child: IconButton(
+                 icon: Icon(Icons.add_a_photo),
+                 onPressed: (){
+                   //_selecionarfoto();
+                 },
+                 ),
+            ),
+          ),
+         ],
+        ),
+      ),
+    );
+
    
   }
 
@@ -123,16 +160,90 @@ Widget fotoperfil(Size sizescreen)
 
 }
 
+_seleccionar(Size sizescreen)
+{
+  return showGeneralDialog(
+      barrierLabel: "Escoger la foto",
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: Duration(milliseconds: 700),
+      context: context,
+      pageBuilder: (context, anim1, anim2) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: sizescreen.height*0.28,
+            width: sizescreen.width*0.9,
+            child: Stack(
+              children: [
 
+                SizedBox.expand(
+                  child: Image(
+                    image: AssetImage('logo.png'),
+                  ),
+                ),
+                Positioned(
+                  bottom: 25,
+                  left: 30,
 
+                 child: Material(
+                   color: Color(0x00000000),
+                      child: IconButton(
+                        icon: Icon(Icons.camera_alt),
+                        iconSize: 100,
+                        onPressed: (){
 
-  _selecionarfoto() async {
-  foto = await ImagePicker.pickImage(source: ImageSource.gallery);
-  setState(() {
-    
-  });
+                        },
+                        
+                      ),
+                  ),
+                ),
+                 Positioned(
+                  bottom: 25,
+                  left: 200,
+
+                 child: Material(
+                   color: Color(0x00000000),
+                      child: IconButton(
+                        icon: Icon(Icons.photo),
+                        iconSize: 100,
+                      
+                        
+                      ),
+                  ),
+                ),
+                
+                
+              ],
+            ),
+            margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        );
+      },
+      transitionBuilder: (context, anim1, anim2, child) {
+        return SlideTransition(
+          position: Tween(begin: Offset(0, 1), end: Offset(0, 0)).animate(anim1),
+          child: child,
+        );
+      },
+    );
+
 }
 
+
+  _selecionarfotoGalery() async {
+  foto = await ImagePicker.pickImage(source: ImageSource.gallery);
+  setState(() {
+  });
+
+}
+_selecionarfotoCamara() async {
+   foto = await ImagePicker.pickImage(source: ImageSource.camera);
+}
 
    Widget appMenu(Size _screenSize) {
   return AppBar(
