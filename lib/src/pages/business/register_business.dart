ae82961cd18d3eca0adfef117f84c89e63cde189
deivitrix <67457502+deivitrix@users.noreply.github.com>
2020-11-05 16:37:36 -0500

    update

--- lib/src/pages/business/register_business.dart
+++ lib/src/pages/business/register_business.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -40,7 +39,7 @@ Widget fotoperfil(Size sizescreen)
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(40.0),
                 child: FadeInImage(
-                 placeholder: AssetImage('assets/jar-loading.gif'),
+                 placeholder: AssetImage('assets/placeholder.png'),
                  image: new NetworkImage("http://54.197.83.249/Contenido_ftp/Imagenes%20por%20defecto/Placeholder_male.png"),
               fit: BoxFit.cover
               ),
@@ -68,14 +67,68 @@ Widget fotoperfil(Size sizescreen)
 
       ),
     );
+}
+
+
+ Widget _mostrarfotoperfil(Size sizescreen)
+{
+  if(foto!=null)
+  {
+   return Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Stack(
+         children: [
+           Container(
+             width: sizescreen.width*0.3,
+            height: sizescreen.height*0.2,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: FadeInImage(
+                 placeholder: AssetImage('assets/jar-loading.gif'),
+                 image: FileImage(foto),
+              fit: BoxFit.cover
+              ),
+            ),
+             decoration:  BoxDecoration(
+              shape: BoxShape.circle, 
+            ),
+           ),
+           Positioned(
+              bottom: -11,
+              right: -15,
+               child: Container(
+                 child: IconButton(
+                 icon: Icon(Icons.add_a_photo),
+                 onPressed: (){
+                   _selecionarfoto();
+                 },
+                 ),
+            ),
+          ),
+         ],
+        ),
+      ),
+    );
+
+
+  }
+  else
+  {
+   
+  }
+
+
 
 }
 
 
-Future  _selecionarfoto() async {
-  var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-  this.setState(() {
-    foto=picture;
+
+
+  _selecionarfoto() async {
+  foto = await ImagePicker.pickImage(source: ImageSource.gallery);
+  setState(() {
+    
   });
 }
 
